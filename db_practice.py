a78449c7121a3677e1637d2aec625d4a9f03e316
Andrey <adulub@spinupper.com>
2025-04-21 09:07:55 -0400

    v1.2

---
+++ db_practice.py
@@ -0,0 +1,85 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+import logging
+import sqlite3
+from dotenv import load_dotenv
+
+class Form(StatesGroup):
+    name = State()
+    age = State()
+    grade = State()
+
+def init_db():
+    conn = sqlite3.connect('school_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+	CREATE TABLE IF NOT EXISTS students (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	age INTEGER NOT NULL,
+	grade TEXT NOT NULL)
+	''')
+    conn.commit()
+    conn.close()
+
+logging.basicConfig(level=logging.INFO)
+
+init_db()
+
+load_dotenv('.env')
+API_TOKEN = os.getenv("API_TOKEN")
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message, state: FSMContext):
+    await message.answer("Привет! Как тебя зовут?")
+    await state.set_state(Form.name)
+
+# /help
+@dp.message(Command("help"))
+async def help_handler(message: Message):
+    help_text = (
+        "Бот поддерживает команды /start и /help\n"
+        "Бот просит пользователя ввести его имя, возраст и класс обучения\n"
+        "и далее сохраняет полученную информацию в базу данных."
+    )
+    await message.answer(help_text)
+
+@dp.message(Form.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Сколько тебе лет?")
+    await state.set_state(Form.age)
+
+@dp.message(Form.age)
+async def age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("В каком классе ты учишься?")
+    await state.set_state(Form.grade)
+
+@dp.message(Form.grade)
+async def grade(message: Message, state:FSMContext):
+    await state.update_data(grade=message.text)
+    student_data = await state.get_data()
+    conn = sqlite3.connect('school_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+       INSERT INTO students (name, age, grade) VALUES (?, ?, ?)''',
+                (student_data['name'], student_data['age'], student_data['city']))
+    conn.commit()
+    conn.close()
+    await message.answer("Информация сохранена в базе данных")
+    await state.clear()
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
